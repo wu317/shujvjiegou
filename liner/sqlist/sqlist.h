7358467e70af15fefdbed560aa67a6a888e9e9cc
wzr <317410Wu@gmail.com>
2024-11-08 16:46:11 +0800

    commit from vscode

--- liner/sqlist/sqlist.h
+++ liner/sqlist/sqlist.h
@@ -1,11 +1,13 @@
+/*
+typedef int data_t;
+#define N 128
 
-/*struct sqlist_t {
+struct sqlis_t {
     data_t data[N];
     int last;
 };
-
-typedef struct sqlist_t sqlist;//now:sqlist L;ago:struct sqlist_t L;
-typedef struct sqlist_t * sqlink;//now:sqlink * p;ago:struct sqlist_t * p;
+typedef struct sqlist_t sqlist;  //sqlist L;struct sqlist_t L;
+typedef struct sqlist_t * sqlink;//sqlink p;struct sqlist_t *p;
 */
 
 typedef int data_t;
@@ -17,14 +19,5 @@ typedef struct {
 }sqlist,*sqlink;
 
 
-sqlink list_create(void); 
 
-int list_clear(sqlink L);
 
-int list_empty(sqlink L);
-
-int list_length(sqlink L);
-
-int list_locate(sqlink L,data_t value);
-
-int list_insert(sqlink L,data_t value,int pos);
