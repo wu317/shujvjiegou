db11d7967aa919dc6f4b025cae2587b570f6257b
wzr <317410Wu@gmail.com>
2024-11-24 00:12:29 +0800

    commit from vscode

--- stack/sqstack/sqstack.c
+++ stack/sqstack/sqstack.c
@@ -6,49 +6,84 @@
 sqstack * stack_create(int len)
 {
     sqstack * s;
-    if(s = (sqstack *)malloc(sizeof(sqstack)) == NULL)
+    if((s = (sqstack *)malloc(sizeof(sqstack))) == NULL)
     {
         printf("malloc sqstack failed\n");
         return NULL;
     }
-    if(s->data = (data_t *)malloc(len * sizeof(data_t)) == NULL)
+    if((s->data = (data_t *)malloc(len * sizeof(data_t))) == NULL)
     {
         printf("malloc data falied\n");
+        free(s);
         return NULL;
     }
-    memrset(s->data,0,len*sizeof(data_t));
+    memset(s->data,0,len*sizeof(data_t));
     s->maxlen = len;
     s->top = -1;
     return s;
 }
 int stack_push(sqstack *s, data_t value)
 {
+    if(s == NULL)
+    {
+        printf("s is NULL\n");
+        return -1;
+    }
+    if(s->top == s->maxlen -1)
+    {
+        printf("stack is full\n");
+        return -1;
+    }
+    s->top++;
+    s->data[s->top] = value;
     return 0;
 }
 int stack_empty(sqstack *s)
 {
-    return 0;
+    if(s == NULL)
+    {
+        printf("s is NULL\n");
+        return -1;
+    }
+    return (s->top == -1) ? 1 : 0;
 }
 int stack_full(sqstack *s)
 {
-    return 0;
+    if(s == NULL)
+    {
+        printf("s is NULL\n");
+        return -1;
+    }
+    return ((s->top == s->maxlen-1) ? 1 : 0);
 }
 data_t stack_pop(sqstack *s)
 {
-    return 0;
+    s->top--;
+    return (s->data[s->top+1]);
 }
 data_t stack_top(sqstack *s)
 {
-    return 0;
+    return (s->data[s->top]);
 }
 int stack_clear(sqstack *s)
 {
+    if(s == NULL)
+    {
+        printf("s is NULL\n");
+        return -1;
+    }
+    s->top = -1;
     return 0;
 }
 int stack_free(sqstack *s)
 {
+    if(s == NULL)
+    {
+        printf("s is NULL\n");
+        return -1;
+    }
+    if(s->data != NULL)
+        free(s->data);
+    free(s);
     return 0;
 }
-
-
-
--- stack/sqstack/test.c
+++ stack/sqstack/test.c
@@ -4,11 +4,20 @@
 int main()
 {
     sqstack *s;
+
     s = stack_create(100);
     if(s == NULL)
     {
         printf("stack create failed\n");
         return -1;
     }
+    stack_push(s,10);
+    stack_push(s,20);
+    stack_push(s,30);
+    while(!stack_empty(s))
+    {
+        printf("pop:%d\n",stack_pop(s));
+    }
+    stack_free(s);
     return 0;
 }
